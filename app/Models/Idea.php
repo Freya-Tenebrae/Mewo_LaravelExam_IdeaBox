72d25278016423f574dec5017fb85b696fb6a16b
Célia MAGINOT <celia.maginot@mewo-campus.fr>
2025-11-07 13:58:12 +0100

    add modele Idea

---
+++ app/Models/Idea.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Idea extends Model
+{
+    protected $fillable = ['title', 'description', 'user_id'];
+
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(),
+            'content' => $this->faker->paragraph(),
+            'user_id' => \App\Models\User::factory(),
+        ];
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head> 
     <meta charset="UTF-8"> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-    <title>Idea Box</title> 
+    <title>Boite à Idées</title> 
     <link 
 href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
 rel="stylesheet"> 
@@ -11,7 +11,7 @@ rel="stylesheet">
 <body> 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
-            <a class="navbar-brand" href="/">Idea Box</a>
+            <a class="navbar-brand" href="/">Boite à Idées</a>
             <div class="collapse navbar-collapse">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                     @auth
